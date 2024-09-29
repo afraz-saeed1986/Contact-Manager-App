59b4f6f1be589a41ecedc167ca22b21238d13030
s.afraz <s.afraz@rayanbourse.ir>
2024-09-29 12:52:21 +0330

    push to main

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './App.css';
+import {AddContact,EditContact,ViewContact,Contacts,Contact,Navbar} from './components';
+import {Route,Routes,Navigate} from 'react-router-dom'
+
+
+
+const App = () => {
+  const [getContacts,setContacts] = useState([]);
+  const [loading,setLoading] = useState(false);
+
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+          <Route path='/' element={<Navigate to="/contacts" />} />
+          <Route path='/contacts' element={<Contacts contacts={getContacts} loading={loading} />} />
+          <Route path='/contacts/add' element={<AddContact />} />
+          <Route path='/contacts/:contactId' element={<ViewContact />} />
+          <Route path='/contacts/edit/:contactId' element={<EditContact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
